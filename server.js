c9262d6b2dcf11b47f99e3b49fb04b06d62cb4b8
lotteje <lotje.j@hotmail.com>
2018-07-25 15:18:14 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Install express server
+/* //Install express server
 const express = require('express');
 const path = require('path');
 
@@ -13,4 +13,10 @@ res.sendFile(path.join(__dirname+'/dist/bibsearch-krook/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);*/
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
