b782c39a49a87a3e3b480bce4d3fc1b1845c96ab
lotteje <lotje.j@hotmail.com>
2018-07-25 15:22:19 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ res.sendFile(path.join(__dirname+'/dist/bibsearch-krook/index.html'));
 app.listen(process.env.PORT || 8080);*/
 
 const express = require('express');
+var cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
