84d446046e5ea88473e3d223eea58ea0fe3bcb64
lotteje <lotje.j@hotmail.com>
2018-07-30 12:17:43 +0200

    cors

--- dist/main.js
+++ dist/main.js
@@ -120,7 +120,7 @@ var AppComponent = /** @class */ (function () {
         var temp = form.controls["zoekterm"].value;
         this._zoekterm = temp.replace(/\s/g, "%20");
         if (typeof this._zoekterm !== "undefined") {
-            var url = 'https://cors-anywhere.herokuapp.com/http://gent.staging.aquabrowser.be/api/v1/search/?q=' + this._zoekterm + '&authorization=' + _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].apikey;
+            var url = 'http://gent.staging.aquabrowser.be/api/v1/search/?q=' + this._zoekterm + '&authorization=' + _environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].apikey;
             this._http.get(url, {
                 headers: new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpHeaders"]()
                     .set('Content-Type', 'text/xml')
--- server.js
+++ server.js
@@ -14,5 +14,12 @@ const app = express();
 // });
 
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
